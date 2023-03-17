db927ebab703503431202a6d3fcbc49fb7ab0775
Fatima <fh356165@gmail.com>
2023-03-17 09:29:51 -0600

    longitud-delete

--- Longitud/UnidadLongitud.php
+++
@@ -1,8 +0,0 @@
-<?php
-    // Definimos la interfaz UnidadLongitud que deben implementar todas las clases de unidades de volumen
-    interface UnidadLongitud {
-        public function convertir_a_metros(UnidadLongitud   $unidad_desde, $valor);
-       public function convertir_desde_metros(UnidadLongitud  $unidad_hasta,$valor);
-        
-    }
-?>
